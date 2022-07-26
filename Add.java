65ac9ce367913d383719d3d35675942403cd3a64
Anand Muragayya Sankrattimath <email>
2022-07-26 14:33:22 +0530

    another task is uploaded

---
+++ Add.java
@@ -0,0 +1,11 @@
+class Add{
+int add(int num1,int num2){
+    int num3 = num1+num2;
+    return num3;
+}
+
+public static void main(String args[]){
+    int addition = add(2,3);
+    System.out.println(addition);
+}
+}
